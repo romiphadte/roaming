deab85d5a816952cd1e0b16e9583e657634771f5
Romi Phadte <romiphadte@gmail.com>
2014-07-19 14:47:04 -0700

    init files

---
+++ roaming/roaming/YOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YOAppDelegate.h
+//  roaming
+//
+//  Created by Romi Phadte on 7/19/14.
+//  Copyright (c) 2014 Romi Phadte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ roaming/roaming/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  roaming
+//
+//  Created by Romi Phadte on 7/19/14.
+//  Copyright (c) 2014 Romi Phadte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "YOAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YOAppDelegate class]));
+    }
+}
